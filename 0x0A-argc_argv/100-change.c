3657163ed22a104c8de5af01d87a29e33b046ea5
elakhmad2020 <ahmadabdullahibayero@gmail.com>
2023-04-10 08:08:15 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,86 @@
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+
+
+
+
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * coin_count - counts the min coins needed for change
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * @argv: argument vector
+ * @count: int to count coins
+ * Return: int
  */
-int _putchar(char c)
+int coin_count(char *argv[], int count)
 {
-	return (write(1, &c, 1));
+int sum = 0;
+
+while (!(sum + 25 > atoi(argv[1])))
+{
+sum += 25;
+count++;
+}
+while (!(sum + 10 > atoi(argv[1])))
+{
+sum += 10;
+count++;
+}
+while (!(sum + 5 > atoi(argv[1])))
+{
+sum += 5;
+count++;
+}
+while (!(sum + 2 > atoi(argv[1])))
+{
+sum += 2;
+count++;
+}
+while (!(sum + 1 > atoi(argv[1])))
+{
+sum += 1;
+count++;
+}
+return (count);
+}
+
+
+
+
+
+
+
+
+/**
+ * main - prints the min num of coins to make change for an amount of money
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+int count = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+else if (atoi(argv[1]) < 0)
+{
+printf("%d\n", 0);
+return (0);
+}
+count = coin_count(argv, count);
+printf("%d\n", count);
+return (0);
 }
 
