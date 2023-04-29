f3414e4ebd6f064eb8f07425d7f0105c09f71ef0
elakhmad2020 <ahmadabdullahibayero@gmail.com>
2023-04-28 23:21:29 -0700

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
+*main - prints the word putchar
+*
+*Return: 0 on success execution.
+*/
+
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
 
 	return (0);
 }
