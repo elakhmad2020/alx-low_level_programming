1cf29af85defa87387d86a59f4339798a7950f38
elakhmad2020 <ahmadabdullahibayero@gmail.com>
2023-04-29 00:13:52 -0700

    Learnt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
-*main - prints the word putchar
-*
-*Return: 0 on success execution.
-*/
+ * main - print _putchar
+ * is written by Ahmad
+ * Return: 0
+ */
 
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
