139ba6baa3c0ced6286e899e8bd84393f4d50d64
elakhmad2020 <ahmadabdullahibayero@gmail.com>
2023-05-02 16:04:17 -0700

    Now

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,61 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_add - Adds two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Sum of a and b
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_sub - Substracts two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Substraction of a and b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mul - Multiplies two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Multiplication of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_div - Divides two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Division of a and b
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
 /**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mod - Modulus of two numbers
+ * @a: First Number
+ * @b: Second Number
+ *
+ * Return: Modulus of a and b
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
