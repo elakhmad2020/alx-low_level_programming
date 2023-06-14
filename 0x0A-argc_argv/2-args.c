b2a1d88241f9463658b49ceda5a8b21fd03993e3
root <ahmadabdullahibayero@gmail.com>
2023-06-14 13:37:41 -0700

    Operation

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,9 +10,10 @@
 int main (int argc, char *argv[])
 {
 	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-
-	return (0)
+	printf("%d\n", argc);
+	for (i=0; i<argc-1;i++)
+	{
+		printf("%s", *argv[i]);
+	}
+	return 0;
 }
