5eb1441df3e65c8154f62d547e925c7c21b5c022
root <ahmadabdullahibayero@gmail.com>
2023-06-14 13:06:42 -0700

    Operations

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - Prints all arguments it receives
- * @argc: - size of argv array and number of command line arguments
- * @argv: - An array of size argc
+ * main - Prints all arguments passed into main
+ * @argc: - Number of command line arguments
+ * @argv: - Array name
  * Return: 0
  */
 
@@ -11,9 +12,7 @@ int main (int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
-	}
 
 	return (0)
 }
