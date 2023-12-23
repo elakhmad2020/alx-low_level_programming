e603e7c60f4a8b901f280a2e58b52b4693b34121
elakhmad2020 <ahmadabdullahibayero@gmail.com>
2023-12-23 00:27:11 -0800

    Yes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,9 @@
-#include "function_pointers.h"
+/* function_pointers.h */
 
-/**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
- *
- * Return: no return.
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-		f(name);
-}
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
+
